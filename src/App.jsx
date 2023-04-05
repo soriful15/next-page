46adce8b78958326a2192fcf2092f61a1678e737
soriful15 <sorifulislam.rs10@gmailcomm>
2023-04-05 15:26:34 +0600

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from './Components/Header/Header';
+import Footer from './Components/Footer';
 
 const App = () => {
   return (
     <div>
      <Header></Header>
     {/* header */}
+    <div className='min-h-[calc(100vh-136px)]'>
     <Outlet></Outlet>
+    </div>
     {/* footer */}
+    <Footer></Footer>
     </div>
   );
 };
